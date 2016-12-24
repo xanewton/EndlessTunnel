e400f1df05d5840bcae46d276c56642735c8f285
Angel Newton <xanewton@gmail.com>
2016-12-24 14:28:29 -0600

    Add license file

--- app/src/main/cpp/dialog_scene.hpp
+++ app/src/main/cpp/dialog_scene.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Google Inc.
+ * Copyright (C) Angel Garcia
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
